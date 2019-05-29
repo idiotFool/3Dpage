be75cfce744f2da2dc74b45e8a2884a948aa3e6b
daichao <15229395912@163.com>
2019-05-29 21:54:54 +0800

    add solid line

--- 3DTest/index.js
+++ 3DTest/index.js
@@ -9,34 +9,25 @@ require(["../Cesium-1.57/Source/Cesium.js"], (Cesium) => {
                 -125, 45, 500000
             ]),
             width : 2,
-            /*material : new Cesium.PolylineDashMaterialProperty({
+            material : new Cesium.PolylineDashMaterialProperty({
                 color: Cesium.Color.CYAN,  // 青色; 蓝绿色
                 dashPattern: 43690.0,
                 dashLength: 50
-            })*/
+            })
         }
     })
 
-    /*dashLine.polyline.material = Cesium.Material.fromType('PolylineDash', {
-        color : new Cesium.Color(1.0, 0.0, 0.0, 1.0)
-    })*/
-
-
-    viewer.scene.primitives.add(new Cesium.Primitive({
-        geometryInstances: new Cesium.GeometryInstance({
-            geometry : new Cesium.PolylineGeometry({
-                positions : Cesium.Cartesian3.fromDegreesArrayHeights([
-                    -75, 45, 500000,
-                    -125, 45, 500000
-                ]),
-                width : 10.0,
-                vertexFormat : Cesium.PolylineMaterialAppearance.VERTEX_FORMAT
-            })
-        }),
-        appearance: new Cesium.PolylineMaterialAppearance({
-            material:Cesium.Material.fromType('PolylineDash')
-        })
-    }));
+    const solidLine = viewer.entities.add({
+        name: 'solid line',
+        polyline: {
+            positions: Cesium.Cartesian3.fromDegreesArrayHeights([
+                -75, 43, 500000,
+                -125, 43, 500000
+            ]),
+            width: 2,
+            material: Cesium.Color.WHITE
+        }
+    });
 
     viewer.zoomTo(dashLine);
 })
